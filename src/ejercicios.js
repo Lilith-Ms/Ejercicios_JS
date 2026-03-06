c0a2de336bfb07f2ed44119e48668706cd6eae1b
Lilith-Ms <jaz.chik.283@gmail.com>
2026-03-05 20:25:09 -0500

    feat: Ok 25

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -154,7 +154,9 @@ function numeroRandom() {return Math.random ()
 }
 
 // Test 25
-function esPositivo(numero) {
+function esPositivo(numero) {if (numero > 0) return "Es positivo"
+  if (numero < 0) return "Es negativo"
+  return false
 	//La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo.
 	//Si el número es positivo, devolver ---> "Es positivo"
 	//Si el número es negativo, devolver ---> "Es negativo"
