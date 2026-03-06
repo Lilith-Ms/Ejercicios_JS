764eac3e8365a2976d59f894884f0cf2ce8c4293
Lilith-Ms <jaz.chik.283@gmail.com>
2026-03-05 20:26:42 -0500

    feat: Ok 26

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -165,7 +165,7 @@ function esPositivo(numero) {if (numero > 0) return "Es positivo"
 }
 
 // Test 26
-function agregarSimboloExclamacion(str) {
+function agregarSimboloExclamacion(str) {return (str) + ("!")
 	// Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
 	// Ejemplo: "hello world" pasaría a ser "hello world!"
 	// Tu código:
